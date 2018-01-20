02d87641c770a2ef219fedee1199569134abbe3e
KimBoWoon <haioer@naver.com>
2018-01-20 20:00:42 +0900

    init

---
+++ src/main/java/com/logdata/logcat/LogcatApplication.java
@@ -0,0 +1,12 @@
+package com.logdata.logcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogcatApplication.class, args);
+	}
+}
