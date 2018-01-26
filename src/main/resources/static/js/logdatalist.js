62c1c42398c2218a346562e0ead28e4a919fc0cd
KimBoWoon <haioer@naver.com>
2018-01-27 00:46:01 +0900

    react.js 적용은 했지만 필터는 나중에

--- src/main/resources/static/js/logdatalist.js
+++ src/main/resources/static/js/logdatalist.js
@@ -2,83 +2,68 @@
     window.App = window.App || { Components: {} };
 
     var LogDataListRow = React.createClass({
-        // verbLevel: function (props) {
-        //     return (
-        //         <div className="panel panel-success">
-        //             <div className="panel-heading">Verb</div>
-        //             <div className="panel-body">
-        //                 <p>Time : {props.logData.time}</p>
-        //                 <p>Tag : {props.logData.tag}</p>
-        //                 <p>Message : {props.logData.message}</p>
-        //                 <p>Level : {props.logData.level}</p>
-        //             </div>
-        //         </div>
-        //     );
-        // },
-        // debegLevel: function (props) {
-        //     return (
-        //         <div className="panel panel-primary">
-        //             <div className="panel-heading">Debug</div>
-        //             <div className="panel-body">
-        //                 <p>Time : {props.logData.time}</p>
-        //                 <p>Tag : {props.logData.tag}</p>
-        //                 <p>Message : {props.logData.message}</p>
-        //                 <p>Level : {props.logData.level}</p>
-        //             </div>
-        //         </div>
-        //     );
-        // },
-        // infomationLevel: function (props) {
-        //     return (
-        //         <div className="panel panel-info">
-        //             <div className="panel-heading">Info</div>
-        //             <div className="panel-body">
-        //                 <p>Time : {props.logData.time}</p>
-        //                 <p>Tag : {props.logData.tag}</p>
-        //                 <p>Message : {props.logData.message}</p>
-        //                 <p>Level : {props.logData.level}</p>
-        //             </div>
-        //         </div>
-        //     );
-        // },
-        // errorLevel: function (props) {
-        //     return (
-        //         <div className="panel panel-danger">
-        //             <div className="panel-heading">Error</div>
-        //             <div className="panel-body">
-        //                 <p>Time : {props.logData.time}</p>
-        //                 <p>Tag : {props.logData.tag}</p>
-        //                 <p>Message : {props.logData.message}</p>
-        //                 <p>Level : {props.logData.level}</p>
-        //             </div>
-        //         </div>
-        //     );
-        // },
-        // warningLevel: function (props) {
-        //     return (
-        //         <div className="panel panel-warning">
-        //             <div className="panel-heading">Warning</div>
-        //             <div className="panel-body">
-        //                 <p>Time : {props.logData.time}</p>
-        //                 <p>Tag : {props.logData.tag}</p>
-        //                 <p>Message : {props.logData.message}</p>
-        //                 <p>Level : {props.logData.level}</p>
-        //             </div>
-        //         </div>
-        //     );
-        // },
         render: function () {
-            return (
-                <div className="panel panel-success">
-                    <div className="panel-heading">Verb</div>
-                    <div className="panel-body">
-                        <p>Time : {this.props.logData.stringTime}</p>
-                        <p>Tag : {this.props.logData.tag}</p>
-                        <p>Message : {this.props.logData.message}</p>
-                        <p>Level : {this.props.logData.level}</p>
+            if (this.props.logData.level === 'v') {
+                return (
+                    <div className="panel panel-success">
+                        <div className="panel-heading">Verb</div>
+                        <div className="panel-body">
+                            <p>Time : {this.props.logData.stringTime}</p>
+                            <p>Tag : {this.props.logData.tag}</p>
+                            <p>Message : {this.props.logData.message}</p>
+                            <p>Level : {this.props.logData.level}</p>
+                        </div>
                     </div>
-                </div>
-            );
+                );
+            } else if (this.props.logData.level === 'w') {
+                return (
+                    <div className="panel panel-warning">
+                        <div className="panel-heading">Warning</div>
+                        <div className="panel-body">
+                            <p>Time : {this.props.logData.stringTime}</p>
+                            <p>Tag : {this.props.logData.tag}</p>
+                            <p>Message : {this.props.logData.message}</p>
+                            <p>Level : {this.props.logData.level}</p>
+                        </div>
+                    </div>
+                );
+            } else if (this.props.logData.level === 'i') {
+                return (
+                    <div className="panel panel-info">
+                        <div className="panel-heading">Info</div>
+                        <div className="panel-body">
+                            <p>Time : {this.props.logData.stringTime}</p>
+                            <p>Tag : {this.props.logData.tag}</p>
+                            <p>Message : {this.props.logData.message}</p>
+                            <p>Level : {this.props.logData.level}</p>
+                        </div>
+                    </div>
+                );
+            } else if (this.props.logData.level === 'd') {
+                return (
+                    <div className="panel panel-primary">
+                        <div className="panel-heading">Debug</div>
+                        <div className="panel-body">
+                            <p>Time : {this.props.logData.stringTime}</p>
+                            <p>Tag : {this.props.logData.tag}</p>
+                            <p>Message : {this.props.logData.message}</p>
+                            <p>Level : {this.props.logData.level}</p>
+                        </div>
+                    </div>
+                );
+            } else if (this.props.logData.level === 'e') {
+                return (
+                    <div className="panel panel-danger">
+                        <div className="panel-heading">Error</div>
+                        <div className="panel-body">
+                            <p>Time : {this.props.logData.stringTime}</p>
+                            <p>Tag : {this.props.logData.tag}</p>
+                            <p>Message : {this.props.logData.message}</p>
+                            <p>Level : {this.props.logData.level}</p>
+                        </div>
+                    </div>
+                );
+            }
         }
     });
 
