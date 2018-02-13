d4175cc39cd3e080c2292838f4cdb92fa8930024
KimBoWoon <haioer@naver.com>
2018-02-14 00:49:53 +0900

    mypage에 apiKey 보이기

--- backend/src/main/java/com/logdata/backend/controller/HelpController.java
+++ backend/src/main/java/com/logdata/backend/controller/HelpController.java
@@ -1,5 +1,8 @@
 package com.logdata.backend.controller;
 
+import com.logdata.backend.model.UserVO;
+import com.logdata.backend.repository.UserDataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,17 +10,34 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
+import java.security.Principal;
 import java.util.HashMap;
 
 @Controller
 public class HelpController {
+    @Autowired
+    private UserDataRepository userDataRepository;
+
     @RequestMapping(value = "/help", method = RequestMethod.GET)
     public String help() {
         return "help";
     }
 
     @RequestMapping(value = "/mypage", method = RequestMethod.GET)
-    public String myPage() {
+    public String myPage(Principal user, Model model) {
+        if (user == null) {
+            return "login";
+        }
+
+        String apiKey = this.userDataRepository.findByUserID(user.getName()).getApiKey();
+
+        model.addAttribute("apikey", apiKey);
+
         return "mypage";
+    }
+
+    public String getUserApiKey(Principal user) {
+        UserVO u = this.userDataRepository.findByUserID(user.getName());
+        return u.getApiKey();
     }
 }
