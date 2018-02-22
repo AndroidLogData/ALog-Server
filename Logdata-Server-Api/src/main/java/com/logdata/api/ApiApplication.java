5af91636a720e47c401b66d3bd2ba27089eef33b
KimBoWoon <haioer@naver.com>
2018-02-23 01:10:36 +0900

    commponent scan 수정

--- Logdata-Server-Api/src/main/java/com/logdata/api/ApiApplication.java
+++ Logdata-Server-Api/src/main/java/com/logdata/api/ApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.logdata")
+@ComponentScan("com.logdata.common")
 public class ApiApplication {
 
 	public static void main(String[] args) {
