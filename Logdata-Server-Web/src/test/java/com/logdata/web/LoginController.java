4c6f34d08f0ed4a531e2589dac8f427033a32a26
KimBoWoon <haioer@naver.com>
2018-02-28 21:01:44 +0900

    logdata server web 추가

---
+++ Logdata-Server-Web/src/test/java/com/logdata/web/LoginController.java
@@ -0,0 +1,119 @@
+package com.logdata.web;
+
+import com.logdata.common.model.UserVO;
+import com.logdata.common.repository.UserDataRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginController {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private UserDataRepository userDataRepository;
+
+    @Test
+    public void loginPageGetTest() throws Exception {
+        // when
+        MockHttpServletResponse response = mvc.perform(
+                get("/login")
+//                        .with(
+//                                user("user")
+//                                        .password("user")
+//                                        .roles("USER")
+//                        )
+        ).andDo(print())
+                .andExpect(view().name("login"))
+                .andReturn().getResponse();
+
+        // then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentType()).isEqualTo(MediaType.TEXT_HTML_VALUE + ";charset=UTF-8");
+    }
+
+    @Test
+    public void registrationPageGetTest() throws Exception {
+        // when
+        MockHttpServletResponse response = mvc.perform(
+                get("/registration")
+//                        .with(
+//                                user("user")
+//                                        .password("user")
+//                                        .roles("USER")
+//                        )
+        ).andDo(print())
+                .andExpect(view().name("registration"))
+                .andReturn().getResponse();
+
+        // then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentType()).isEqualTo(MediaType.TEXT_HTML_VALUE + ";charset=UTF-8");
+    }
+
+    @Test
+    public void registrationPostTest() throws Exception {
+        // when
+        MockHttpServletResponse response = mvc.perform(
+                post("/registration")
+                        .param("username", "user")
+                        .param("password", "user")
+                        .with(csrf())
+//                        .with(
+//                                user("user")
+//                                        .password("user")
+//                                        .roles("USER")
+//                        )
+        ).andDo(print())
+                .andExpect(view().name("login"))
+                .andReturn().getResponse();
+
+        // then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentType()).isEqualTo(MediaType.TEXT_HTML_VALUE + ";charset=UTF-8");
+    }
+
+    @Test
+    public void registrationPostSameIDTest() throws Exception {
+        when(userDataRepository.findByUserID("user")).thenReturn(new UserVO("user", "user"));
+        // when
+        MockHttpServletResponse response = mvc.perform(
+                post("/registration")
+                        .param("username", "user")
+                        .param("password", "user")
+                        .with(csrf())
+//                        .with(
+//                                user("user")
+//                                        .password("user")
+//                                        .roles("USER")
+//                        )
+        ).andDo(print())
+                .andExpect(model().attribute("sameID", true))
+                .andExpect(view().name("registration"))
+                .andReturn().getResponse();
+
+        // then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentType()).isEqualTo(MediaType.TEXT_HTML_VALUE + ";charset=UTF-8");
+    }
+}
---
+++ Logdata-Server-Web/src/test/java/com/logdata/web/MainControllerTest.java
@@ -0,0 +1,86 @@
+package com.logdata.web;
+
+import com.logdata.common.model.LogVO;
+import com.logdata.common.model.UserVO;
+import com.logdata.common.repository.LogDataRepository;
+import com.logdata.common.repository.UserDataRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MainControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private LogDataRepository logDataRepository;
+    @MockBean
+    private UserDataRepository userDataRepository;
+
+    @Test
+    public void indexPageGetTest() throws Exception {
+        // when
+        MockHttpServletResponse response = mvc.perform(
+                get("/")
+//                        .with(
+//                                user("user")
+//                                        .password("user")
+//                                        .roles("USER")
+//                        )
+        ).andDo(print())
+                .andExpect(view().name("index"))
+                .andReturn().getResponse();
+
+        // then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentType()).isEqualTo(MediaType.TEXT_HTML_VALUE + ";charset=UTF-8");
+    }
+
+    @Test
+    public void mainPageGetTest() throws Exception {
+        List<LogVO> logVOs = new ArrayList<>();
+        UserVO user = new UserVO("user", "user");
+        when(userDataRepository.findByUserID("user")).thenReturn(user);
+        when(logDataRepository.findByApiKeyAndPackageNameAndLevel(user.getApiKey(), "android", "v")).thenReturn(logVOs);
+        when(logDataRepository.findByApiKeyAndPackageNameAndLevel(user.getApiKey(), "android", "i")).thenReturn(logVOs);
+        when(logDataRepository.findByApiKeyAndPackageNameAndLevel(user.getApiKey(), "android", "d")).thenReturn(logVOs);
+        when(logDataRepository.findByApiKeyAndPackageNameAndLevel(user.getApiKey(), "android", "w")).thenReturn(logVOs);
+        when(logDataRepository.findByApiKeyAndPackageNameAndLevel(user.getApiKey(), "android", "e")).thenReturn(logVOs);
+        // when
+        MockHttpServletResponse response = mvc.perform(
+                get("/main")
+                        .with(
+                                user("user")
+                                        .password("user")
+                                        .roles("USER")
+                        )
+        ).andDo(print())
+                .andReturn().getResponse();
+
+        // then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentType()).isEqualTo(MediaType.APPLICATION_JSON_UTF8.toString());
+        assertThat(response.getContentAsString()).isEqualTo("{\"logData\":[]}");
+    }
+}
