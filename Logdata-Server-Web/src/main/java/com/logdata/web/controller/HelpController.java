c9b431f2f13c086b2d2bc5692180536ac4e3351a
KimBoWoon <haioer@naver.com>
2018-03-20 14:35:54 +0900

    modify delete method

--- Logdata-Server-Web/src/main/java/com/logdata/web/controller/HelpController.java
+++ Logdata-Server-Web/src/main/java/com/logdata/web/controller/HelpController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 import java.util.ArrayList;
@@ -33,6 +34,24 @@ public class HelpController {
         if (user == null) {
             return "login";
         }
+
+        LinkedHashMap<String, Integer> crashList = restAPIUtility.getCrashList("/mypage", getUserApiKey(user.getName()));
+        ArrayList<LogDataInfoVO> logDataInfoVO = new ArrayList<>(Arrays.asList(restAPIUtility.getLogDataInfo("/main", getUserApiKey(user.getName()))));
+
+        model.addAttribute("crashList", crashList);
+        model.addAttribute("logDataInfo", logDataInfoVO);
+        model.addAttribute("apikey", getUserApiKey(user.getName()));
+
+        return "mypage";
+    }
+
+    @RequestMapping(value = "/logdata/{query}", method = {RequestMethod.POST, RequestMethod.DELETE})
+    public String logDataDelete(Principal user, @RequestParam(value = "packagename") String packageName, Model model) {
+        if (user == null) {
+            return "login";
+        }
+
+        System.out.println(restAPIUtility.deleteLogData("/logdata", getUserApiKey(user.getName()), packageName));
 
         LinkedHashMap<String, Integer> crashList = restAPIUtility.getCrashList("/mypage", getUserApiKey(user.getName()));
         ArrayList<LogDataInfoVO> logDataInfoVO = new ArrayList<>(Arrays.asList(restAPIUtility.getLogDataInfo("/main", getUserApiKey(user.getName()))));
