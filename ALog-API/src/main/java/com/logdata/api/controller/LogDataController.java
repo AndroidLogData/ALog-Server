48586c79dbfeee2017e8720936b38c4eb43d51c7
KimBoWoon <haioer@naver.com>
2018-06-13 15:14:10 +0900

    documention 작성중

--- ALog-API/src/main/java/com/logdata/api/controller/LogDataController.java
+++ ALog-API/src/main/java/com/logdata/api/controller/LogDataController.java
@@ -2,6 +2,7 @@ package com.logdata.api.controller;
 
 import com.logdata.api.sevice.LogDataService;
 import com.logdata.api.sevice.PackageNameDataService;
+import com.logdata.common.logger.Logger;
 import com.logdata.common.model.LogVO;
 import com.logdata.common.model.PackageNameVO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,9 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * 로그 데이터를 관리하는 컨트롤러
+ */
 @Controller
 @CrossOrigin(origins = "http://localhost:8080")
 @RequestMapping(value = "/api")
@@ -29,6 +33,12 @@ public class LogDataController {
         this.packageNameDataService = packageNameDataService;
     }
 
+    /**
+     * 안드로이드에서 전달 받은 로그 데이터를 데이터베이스에 넣는 메소드
+     * @param apiKey 로그 데이터 식별자
+     * @param data   로그 데이터 정보
+     * @return 데이터 베이스에 정보가 정상적으로 삽입 되었는지에 대한 정보
+     */
     @RequestMapping(value = "/log-data", method = RequestMethod.POST)
     public ResponseEntity<Map<String, String>> logDataSave(@RequestHeader(value = "apiKey") String apiKey, @RequestBody LogVO data) {
         HttpHeaders responseHeaders = new HttpHeaders();
@@ -38,7 +48,7 @@ public class LogDataController {
         if (apiKey.equals("")) {
             responseHeaders = new HttpHeaders();
             result.put("result", "Need API Key");
-
+            Logger.e("Need API Key");
             return new ResponseEntity<>(result, responseHeaders, HttpStatus.FORBIDDEN);
         }
 
@@ -72,18 +82,31 @@ public class LogDataController {
 
         responseHeaders = new HttpHeaders();
         result.put("result", "Log Data Transfer Success");
-
+        Logger.i("Log Data Input Success");
         return new ResponseEntity<>(result, responseHeaders, HttpStatus.OK);
     }
 
+    /**
+     * 로그 데이터 제거하는 메소드
+     * @param apiKey      로그 데이터 식별자
+     * @param packageName 지우고 싶은 로그 데이터의 패키지 이름
+     * @return 정상적으로 제거 되었는지 정보
+     */
     @RequestMapping(value = "/log-data/{query}", method = RequestMethod.DELETE)
     @ResponseBody
     public ResponseEntity<Map<String, String>> deleteLogData(@RequestHeader(value = "apiKey") String apiKey, @RequestParam(value = "package-name") String packageName) {
+        PackageNameVO packageNameVO = this.packageNameDataService.findPackageNameVOByApiKey(apiKey);
+
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.add("Content-Type", "application/json;charset=UTF-8");
         Map<String, String> result = new HashMap<String, String>();
 
-        logDataService.delete(packageName);
+        for (String item : packageNameVO.getPackageNameList()) {
+            if (item.equals(packageName)) {
+                this.logDataService.delete(item);
+                Logger.i("delete log data success");
+            }
+        }
 
         responseHeaders = new HttpHeaders();
         result.put("result", "Log Data Delete Success");
@@ -91,6 +114,13 @@ public class LogDataController {
         return new ResponseEntity<>(result, responseHeaders, HttpStatus.OK);
     }
 
+    /**
+     * 로그 데이터를 레벨별로 필터링하는 메소드
+     * @param apiKey 로그 데이터 식별자
+     * @param packageName 정보를 찾고 싶은 로그 데이터 패키지 이름
+     * @param level 찾고 싶은 레벨 정보
+     * @return 레벨별로 필터링된 로그 데이터
+     */
     @RequestMapping(value = "/log-data/filter/level/{query}", method = RequestMethod.GET, produces = "application/json")
     @ResponseStatus(value = HttpStatus.OK)
     @ResponseBody
@@ -104,6 +134,13 @@ public class LogDataController {
         return null;
     }
 
+    /**
+     * 로그 데이터를 태크별로 필터링하는 메소드
+     * @param apiKey 로그 데이터 식별자
+     * @param packageName 정보를 찾고 싶은 로그 데이터 패키지 이름
+     * @param tag 찾고 싶은 태그 정보
+     * @return 태그별로 필터링된 로그 데이터
+     */
     @RequestMapping(value = "/log-data/filter/tag/{query}", method = RequestMethod.GET, produces = "application/json")
     @ResponseStatus(value = HttpStatus.OK)
     @ResponseBody
@@ -129,6 +166,12 @@ public class LogDataController {
         return null;
     }
 
+    /**
+     * 로그 데이터를 필터링 할 수 있는 태그 이름 찾는 메소드
+     * @param apiKey 로그 데이터 식별자
+     * @param packageName 태그 데이터를 찾고 싶은 패키지 이름
+     * @return 태그 정보를 담은 Set
+     */
     @RequestMapping(value = "/log-data/tag/set/{query}", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     private Set<String> getTag(@RequestHeader(value = "apiKey") String apiKey, @RequestParam(value = "package-name") String packageName) {
@@ -151,12 +194,23 @@ public class LogDataController {
         return null;
     }
 
+    /**
+     * 로그 데이터를 필터링 할 수 있는 패키지 이름 찾는 메소드
+     * @param apiKey 로그 데이터 식별자
+     * @return 패키지 정보를 담은 ArrayList
+     */
     @RequestMapping(value = "/log-data/package-name/set", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     private ArrayList<String> getPackageName(@RequestHeader(value = "apiKey") String apiKey) {
         return this.packageNameDataService.findPackageNameVOByApiKey(apiKey).getPackageNameList();
     }
 
+    /**
+     * 패키지 이름별로 필터링된 로그 데이터를 찾는 메소드
+     * @param apiKey 로그 데이터 식별자
+     * @param packageName 필터링 하고 싶은 패키지 이름
+     * @return 패키지 이름별로 필터링된 List
+     */
     @RequestMapping(value = "/log-data/filter/package-name/{query}", method = RequestMethod.GET)
     @ResponseBody
     public List<LogVO> logDataPackageNameList(@RequestHeader(value = "apiKey") String apiKey, @RequestParam(value = "package-name") String packageName) {
@@ -167,6 +221,10 @@ public class LogDataController {
         return this.logDataService.findByPackageName(packageName, new Sort(Sort.Direction.DESC, "time"));
     }
 
+    /**
+     * 모든 로그 데이터를 반환하는 메소드
+     * @return 모든 로그 데이터가 담긴 List
+     */
     @RequestMapping(value = "/log-data/list", method = RequestMethod.GET)
     @ResponseBody
     public List<LogVO> allLogData() {
