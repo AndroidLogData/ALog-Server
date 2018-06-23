47012e8410e96be9c25490706e9418b6999db565
KimBoWoon <haioer@naver.com>
2018-06-23 17:30:49 +0900

    build-script 변경 & 차트 색 변경

--- build-script.sh
+++ build-script.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-cd frontend
-sudo npm update
-sudo npm install
-sudo npm run build
-cd ..
-sudo gradle clean build
+os=`cat /etc/issue | head -n 1`
+if [[ `expr "$os" : "\([Ubuntu]*\)"` == "Ubuntu" ]]
+    then {
+        sudo apt-get install -y npm gradle
+        cd frontend
+        sudo npm update
+        sudo npm install
+        sudo npm run build
+        cd ..
+        sudo gradle clean build
+    }
+elif [[ `expr "$os" : "\([CentOS]*\)"` == "CentOS" ]]
+    then {
+        sudo yum install -y npm gradle
+        cd frontend
+        sudo npm update
+        sudo npm install
+        sudo npm run build
+        cd ..
+        sudo gradle clean build
+    }
+fi
+
